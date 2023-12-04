733ca873e229b3830aac4ed530cd56fffad8a8b2
Daniel Whitford <whitford.dsw@gmail.com>
2023-12-04 10:08:31 +0000

    modify gen

--- generators/genfiles.go
+++ generators/genfiles.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strconv"
 	"text/template"
@@ -22,23 +23,78 @@ func (day Day{{.Number}}) SolvePart2() string {
 }
 `
 
+const testTemplate = `package day2
+
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestSolvePart1(t *testing.T) {
+	exampleIn := ""
+
+	want := "-"
+
+	d{{.Number}} := &Day{{.Number}}{}
+	d{{.Number}}.SetInput(exampleIn)
+	got := d{{.Number}}.SolvePart1()
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("TestError() mismatch (-want +got):\n%s", diff)
+	}
+}
+
+func TestSolvePart{{.Number}}(t *testing.T) {
+	exampleIn := ""
+
+	want := ""
+
+	d{{.Number}} := &Day{{.Number}}{}
+	d{{.Number}}.SetInput(exampleIn)
+	got := d{{.Number}}.SolvePart{{.Number}}()
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Errorf("TestError() mismatch (-want +got):\n%s", diff)
+	}
+}
+`
+
+func writeTemplateToFile(filename string, tmpl *template.Template) {
+	_, err := os.Stat(filename)
+	if err == nil {
+		fmt.Printf("Skipping '%s' cos it exists\n", filename)
+		return
+	}
+	if !os.IsNotExist(err) {
+		panic(err)
+	}
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	err = tmpl.Execute(file, struct{ Number int }{Number: i})
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	tmpl, err := template.New("file").Parse(fileTemplate)
+	if err != nil {
+		panic(err)
+	}
+	testTmpl, err := template.New("test").Parse(testTemplate)
 	if err != nil {
 		panic(err)
 	}
 
 	for i := 3; i <= 25; i++ {
 		fileName := "day" + strconv.Itoa(i) + "/day" + strconv.Itoa(i) + ".go"
-		file, err := os.Create(fileName)
-		if err != nil {
-			panic(err)
-		}
-		defer file.Close()
-
-		err = tmpl.Execute(file, struct{ Number int }{Number: i})
-		if err != nil {
-			panic(err)
-		}
+		testFileName := "day" + strconv.Itoa(i) + "/day" + strconv.Itoa(i) + "_test.go"
+		writeTemplateToFile(fileName, tmpl)
+		writeTemplateToFile(testFileName, testTmpl)
 	}
 }
